73e8678151c08682e9c339e706229ee4ed639899
oli414 <oli414@live.nl>
2020-05-10 19:38:53 +0200

    Add module example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// Expose the OpenRCT2 to Visual Studio Code's Intellisense
 /// <reference path="OPENRCT2PATH/bin/openrct2.d.ts" />
 
+// Import a module from another file.
+import Message from "./module";
+
 function main() {
-    console.log("Hello World!");
+    console.log(Message); // Display the imported "Hallo World" message from module.js.
 }
 
 registerPlugin({
